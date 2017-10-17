cba19ff017c1bb1c6d2d93628af98776dbdcbac2
jalc <jantonio2511@gmail.com>
2017-10-17 20:03:18 +0200

    epd3_p2
    añadiendo el titulo de problema en un parrafo

--- EPD3_P2/index.php
+++ EPD3_P2/index.php
@@ -10,6 +10,12 @@ and open the template in the editor.
         <title>EPD3_P2</title>
     </head>
     <body>
+        <p>Cree una página PHP que genere la agenda de una consulta médica en la cual los campos que contendrá serán:
+            médico, año, mes, día y horas disponibles de la agenda del médico de cada día. Inicialmente se deben declarar los vectores que
+            contendrán cada tipo de información, esto es, los nombres de los médicos, años, meses, días cuantos días tiene cada mes y las
+            horas en las que habrá huecos para insertar pacientes, dichas horas. Cree una función que reciba estos vectores e imprima, la
+            agenda del día según el formato de ejemplo para el Dr.Galeno, el día 1 de Enero de 2016:</p>
+        <p>*********************************************************************************************************************</p>
         <?php
         $nombres_medicos = array("Dr.Galeno", "Dr.Poyatos", "Dra.Benavides");
         $horas_pacientes = array("9.00", "9.30", "10.00", "10.30", "11.00", "11.30", "12.00", "12.30", "13.30");
