d45d85dfecf6b5e79ccbf30033db21ef52044118
jalc <jantonio2511@gmail.com>
2017-10-17 19:54:06 +0200

    epd2_p2
    terminado pendiente de corrección antes de subir

--- EPD3_P2/index.php
+++ EPD3_P2/index.php
@@ -7,11 +7,34 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>EPD3_P2</title>
     </head>
     <body>
         <?php
-        echo 'pepe';
+        $nombres_medicos = array("Dr.Galeno", "Dr.Poyatos", "Dra.Benavides");
+        $horas_pacientes = array("9.00", "9.30", "10.00", "10.30", "11.00", "11.30", "12.00", "12.30", "13.30");
         ?>
+        <?php
+
+        function citas($nombres_medicos, $horas_pacientes) {
+
+
+            for ($index1 = 0; $index1 < count($nombres_medicos); $index1++) {
+
+                echo $nombres_medicos[$index1] . ": Citas del día " . date("d") . " de  " . date("M") . " de " . date("Y");
+
+                for ($index3 = 0; $index3 < count($horas_pacientes); $index3++) {
+
+                    echo "<br>" . $horas_pacientes[$index3] . "h";
+                }
+                echo "<br>-------------------------------------------------------";
+                echo "<br>";
+            }
+        }
+        ?>
+        <?php
+        citas($nombres_medicos, $horas_pacientes);
+        ?>
+
     </body>
 </html>
