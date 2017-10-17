70bc6d6413d56f80794bf572b4124bc4a966342c
jalc <jantonio2511@gmail.com>
2017-10-17 20:37:08 +0200

    epd3_p4
    planteando el problema

--- EPD3_P4/index.php
+++ EPD3_P4/index.php
@@ -1,8 +1,27 @@
-<?php
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>EPD3_P4</title>
+    </head>
+    <body>
+        <p>Cree una función PHP a la que se le pase como argumento una matriz con los tiempos de actividad física de varias
+            personas medidos mediante una pulsera inteligente. Existen tres tipos de tiempos: reposo, caminando y corriendo. Esta matriz
+            contendrá en cada fila los tiempos de una persona y en cada columna el tiempo (medido en minutos) en el que ha estado en
+            reposo, caminando y corriendo (tres columnas). La función deberá generar una página web que (haciendo uso de tablas) muestre
+            los tiempos de todas las personas, así como el porcentaje del tiempo total que supone el tiempo en el que cada persona ha estado
+            en reposo. Por último, deberá añadirse una última fila con la media de tiempo por cada tipo de tiempo (reposo, caminando y
+            corriendo) teniendo en cuenta el tiempo de todas las personas. Para comprobar el funcionamiento de la función desarrollada, cree
+            una página PHP que llame a ésta usando una matriz predefinida por usted.</p>
+        <p>***********************************************************************************************************************</p>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+        <?php
+        //matriz como argumento
+        ?>
+    </body>
+</html>
