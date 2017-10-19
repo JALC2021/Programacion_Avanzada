8eb7a2eec7270d41078432493fa3fb325ade053b
jalc <jantonio2511@gmail.com>
2017-10-19 22:55:45 +0200

    epd3_p4
    otro cambio

--- EPD3_P4/index.php
+++ EPD3_P4/index.php
@@ -7,79 +7,104 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>EPD3_P4</title>
     </head>
     <body>
-       <table border="2">
-        <tr>
-            <th colspan="4">
-                Media de notas por alumno
-                </th>
-            </tr>
+        <p>Cree una función PHP a la que se le pase como argumento una matriz con los tiempos de actividad física de varias
+            personas medidos mediante una pulsera inteligente. Existen tres tipos de tiempos: reposo, caminando y corriendo. Esta matriz
+            contendrá en cada fila los tiempos de una persona y en cada columna el tiempo (medido en minutos) en el que ha estado en
+            reposo, caminando y corriendo (tres columnas). La función deberá generar una página web que (haciendo uso de tablas) muestre
+            los tiempos de todas las personas, así como el porcentaje del tiempo total que supone el tiempo en el que cada persona ha estado
+            en reposo. Por último, deberá añadirse una última fila con la media de tiempo por cada tipo de tiempo (reposo, caminando y
+            corriendo) teniendo en cuenta el tiempo de todas las personas. Para comprobar el funcionamiento de la función desarrollada, cree
+            una página PHP que llame a ésta usando una matriz predefinida por usted.</p>
+        <p>***********************************************************************************************************************</p>
+
         <?php
-        $m = array(
-            0 => array(
-                0 => 5,
-                1 => 6,
-                2 => 7
-                
-            ),
-            1 => array(
-                0 => 4,
-                1 => 8,
-                2 => 9
-            ),
-            2 => array(
-                0 => 2,
-                1 => 4,
-                2 => 5
-            ),
-            
-        );
-        
-        function funcion($m){
-            
-            
-            for($i=0;$i<3;$i++){
-                $sum = 0;
-                echo "<tr>";
-                for($j = 0;$j<3;$j++){
-                     echo "<td>";
-                    echo $m[$i][$j];
-                    $sum +=$m[$i][$j];
-                    
-                    echo "</td>";
-                  
+        $persona1 = array(3, 10, 5);
+        $persona2 = array(6, 20, 14);
+        $persona3 = array(10, 15, 30);
+        $matriz = array($persona1, $persona2, $persona3);
+
+
+//        $sum=array_sum($persona1);
+//        $persona1[]=$sum;
+//        foreach ($persona1 as $value) {
+//            echo $value. " ";
+//        }
+        ?>
+        <?php
+
+        function mediaTiempo($m) {
+            $maux = $m;
+
+            $aux_min = array();
+            $minutos = 0;
+
+            for ($nfil = 0; $nfil < count($m); $nfil++) {
+
+                for ($ncol_valor = 0; $ncol_valor < count($m[$nfil]); $ncol_valor++) {
+
+                    $minutos += $m[$nfil][$ncol_valor];
                 }
-                echo "<td>";
-                    echo $sum/3;
-                   
-                    echo "</td>";
-                     
-            echo "</tr>";}
-                
+                $aux_min[$nfil] = $minutos;
+
+                $minutos = 0;
+            }
+
+            for ($index = 0; $index < count($maux); $index++) {
+
+                for ($index1 = 0; $index1 < count($maux); $index1++) {
+                    echo $maux[$index][$index1] . "  ---  ";
+                }
+                echo "<br>";
+            }
+            
+
+//            foreach ($aux_min as $value) {
+//                echo $value . " ";
+//                echo "<br>";
+//            }
+//            echo "<br>";
+        }
+        ?>
+
+        <?php
+
+        function matrizInicial($matriz) {
+            echo "<table border=1>";
+            echo "<caption>Tabla de Datos</caption>";
+            echo "<thead>";
             echo "<tr>";
-            for($i=0;$i<3;$i++){
-                    $sumasig = 0;
-                    
-              
-                    for($j = 0;$j<3;$j++){
-                        
-                         $sumasig += $m[$j][$i];
-                    }
-                    echo "<td>";
-                     echo $sumasig/3;
-                    echo "</td>";
-                     
-    
+            //echo "<th>Persona</th>";
+            echo "<th>Reposo'</th>";
+            echo "<th>Caminando'</th>";
+            echo "<th>Corriendo'</th>";
+            echo "</tr>";
+            echo "</thead>";
+
+
+            foreach ($matriz as $fila) {
+                echo "<tr>";
+                echo"<tbody>";
+                echo "<tr>";
+
+                foreach ($fila as $columna) {
+                    //echo"<td>$fila</td>";
+                    echo "<td>$columna</td>";
+                }
+                echo "</tr>";
             }
             echo "</tr>";
-             
-        } 
-        
-        funcion($m);
+            echo "</tbody>";
+            echo "</table>";
+        }
         ?>
-       </table>
+
+        <?php
+        matrizInicial($matriz);
+        mediaTiempo($matriz);
+        ?>
+
     </body>
 </html>
-
