cc953003a8099a982e2343abfb14d9e8702d1f02
jalc <jantonio2511@gmail.com>
2017-11-05 11:39:43 +0100

    EPD5_P1
    EMPEZANDO

---
+++ EPD_5/p1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Epd_5_p1</title>
+    </head>
+    <body>
+        <p>Cree un sistema, compuesto por varias páginas PHP, que permita gestionar vuelos y destinos de aerolíneas.
+            La primera página del sistema permitirá dar de alta una aerolínea y sus destinos. En ella se pedirá al usuario el nombre de la
+            aerolínea y el número de destinos en los que opera. Al enviar esta información, se proporcionará al usuario un nuevo formulario en
+            el que se podrán indicar, mediante listas desplegables (campos select), las ciudades de cada uno de los destinos. Considere para
+            ello un vector con el siguiente repertorio de ciudades posibles: Roma, Paris, Londres, Dublin, Sevilla, Madrid, Barcelona y
+            Amsterdam. Este repertorio de ciudades deberá ser leído de un archivo de texto que contendrá dichas ciudades separadas por
+            comas (,). Recogidos todos estos datos, se escribirán en dos archivos. El primero almacenará dos campos: un código autonumérico
+            y el nombre de cada aerolínea. El código autonumérico deberá ser generado a partir del siguiente número al máximo de los
+            almacenados en el fichero. El segundo almacenará los nombres de los destinos de cada aerolínea (en forma de pares compuestos
+            por el código numérico de la aerolínea y el nombre del destino). Estos ficheros emplearán el formato CSV para representar los
+            datos, usando un punto y coma (;) como separador entre campos.</p>
+        <?php
+        ?>
+    </body>
+</html>
