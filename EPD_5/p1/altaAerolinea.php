35e7753ed8597e70a4d048bf90233402129a88fd
jalc <jantonio2511@gmail.com>
2017-11-14 21:41:41 +0100

    Revert "lo quiero asi"
    This reverts commit b885906de0309d872b13aadf04f4aa2df4982886.

--- EPD_5/p1/altaAerolinea.php
+++ EPD_5/p1/altaAerolinea.php
@@ -13,9 +13,15 @@ and open the template in the editor.
         <?php
         $nombreAerolinea = $_POST['nombreAerolinea'];
         $nDestinos = $_POST = ['nDestinos'];
+<<<<<<< HEAD
         $f_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'a');
         $LecF_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'r');
         $f = fopen("ciudades.txt", 'r');
+=======
+        $escritura_txt_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'a');
+        $lectura_txt_id_nombreAerolinea = fopen("id_nombreAerolinea.txt", 'r');
+        $lectura_txt_ciudades = fopen("ciudades.txt", 'r');
+>>>>>>> parent of b885906... lo quiero asi
         $id_aerolinea = NULL;
         $vectorIds = array();
 //
@@ -59,6 +65,7 @@ and open the template in the editor.
         }
         ?>
         <h2>Seleccione Destino</h2>
+<<<<<<< HEAD
         <form method="post" action ="altaCompleta.php" name="alta">
             <select multiple size="8" name="vectorCiudadesDestino[]">
                 <?php
@@ -89,6 +96,42 @@ and open the template in the editor.
 
             </select>
 
+=======
+
+        <form method="post" action ="altaCompleta.php" name="alta">
+            <!-- si nDestinos = 3 por ejemplo debe mostrar 3 selec y selecionar uno de cada uno sin que se repita la ciudad para eso debemos eliminar el atributo multiple-->
+            <select multiple size="8" name="vectorCiudadesDestino[]">   
+            <!--<select  size="8" name="vectorCiudadesDestino[]">-->    
+                <?php
+//                $lectura_txt_ciudades = fopen("ciudades.txt", 'r');
+
+                if ($lectura_txt_ciudades) {
+                    flock($lectura_txt_ciudades, LOCK_SH);
+                    $ciudades = fgetcsv($lectura_txt_ciudades, 999, ",");
+                    while (!feof($lectura_txt_ciudades)) {
+                        $ciudades = fgetcsv($lectura_txt_ciudades, 999, ",");
+                        ?>
+                        <option value="Roma"><?php echo $ciudades[0] ?></option>
+                        <option value="Paris"><?php echo $ciudades[1] ?></option>
+                        <option value="Londres"><?php echo $ciudades[2] ?></option>
+                        <option value="Dublin"><?php echo $ciudades[3] ?></option>
+                        <option value="Sevilla"><?php echo $ciudades[4] ?></option>
+                        <option value="Madrid"><?php echo $ciudades[5] ?></option>
+                        <option value="Barcelona"><?php echo $ciudades[6] ?></option>
+                        <option value="Amsterdam"><?php echo $ciudades[7] ?></option>
+                        <?php
+                    }
+
+                    flock($lectura_txt_ciudades, LOCK_UN);
+                    fclose($lectura_txt_ciudades);
+                } else {
+                    echo "error en el fichero";
+                }
+                ?>
+
+            </select>
+
+>>>>>>> parent of b885906... lo quiero asi
             <input type="hidden" name="id_aerolinea" value="<?php echo $id_aerolinea; ?>">
             <input type="hidden" name="nombreAerolinea" value="<?php echo $_POST['nombreAerolinea']; ?>">
             <input type="submit" name="enviarDestino" value="Enviar">
