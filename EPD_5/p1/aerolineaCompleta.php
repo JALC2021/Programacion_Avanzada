dc1148a8a25f2472a0c34e4fc7299db930985f8c
jalc <jantonio2511@gmail.com>
2017-11-18 14:12:52 +0100

    epd5_p1
    falta: En caso de que todos los destinos de la aerolínea hayan sido ya conectados con el destino seleccionado en el primer paso, se mostrará un mensaje de error indicando tal eventualidad.
    
    falta:el tiempo medio de vuelo hacia otros destinos. Esta tabla mostrará los destinos ordenados de forma descendente en función del número de conexiones.

--- EPD_5/p1/aerolineaCompleta.php
+++ EPD_5/p1/aerolineaCompleta.php
@@ -13,11 +13,11 @@ and open the template in the editor.
     <body>
         <?php
         // recibo los datospara finalizar el alta de aerolinea con sus vuelos
-        echo $idAerolinea = $_POST['id_aerolinea'];
-        echo $nombreAerolineaSeleccionada = $_POST['nombreAerolinea'];
-        echo $ciudadOrigen = $_POST['cOrigen'];
-        echo $ciudadDestino = $_POST['cDestino'];
-        echo $duracionVuelo = $_POST['tiempoVuelo'];
+        $idAerolinea = $_POST['id_aerolinea'];
+        $nombreAerolineaSeleccionada = $_POST['nombreAerolinea'];
+        $ciudadOrigen = $_POST['cOrigen'];
+        $ciudadDestino = $_POST['cDestino'];
+        $duracionVuelo = $_POST['tiempoVuelo'];
 
         //escribimos los datos del vuelo completo en vuelos.txt
         $escritura_txt_vuelos = fopen("vuelos.txt", 'a');   //abro escritura
@@ -25,7 +25,32 @@ and open the template in the editor.
         fwrite($escritura_txt_vuelos, $idAerolinea . ";" . $nombreAerolineaSeleccionada . ";" . $ciudadOrigen . ";" . $ciudadDestino . ";" . $duracionVuelo . "\n");
         flock($escritura_txt_vuelos, LOCK_UN);
         fclose($escritura_txt_vuelos);  //cierro escritura
+        //Leemos las aerolineas
+        $nombreAero = array();
+        $lectura_txt_id_nombreAerolinea = fopen("iDnombreAerolinea.txt", 'r');    //modo lectura
+        flock($lectura_txt_id_nombreAerolinea, LOCK_SH);  //bloqueo lectura
+
+        $leerNomAero = fgetcsv($lectura_txt_id_nombreAerolinea, 999, ";");   //lee la primera linea
+        while (!feof($lectura_txt_id_nombreAerolinea)) {
+            $nombreAero[0][] = $leerNomAero[0];
+            $nombreAero[1][] = $leerNomAero[1];
+            $leerNomAero = fgetcsv($lectura_txt_id_nombreAerolinea, 999, ";");
+        }
+        flock($lectura_txt_id_nombreAerolinea, LOCK_UN);
+        fclose($lectura_txt_id_nombreAerolinea);
         ?>
+        <h1>Elija Aerol&iacute;nea</h1>
+        <form method = "post" action = "resumen.php"><?php
+        for ($ind = 0; $ind < count($nombreAero[1]); $ind++) {
+            echo "<article>";
+            ?><input type="radio" name="aerolineaSelec" value="<?php echo $nombreAero[0][$ind] . ";" . $nombreAero[1][$ind]; ?>">
+                <?php
+                echo $nombreAero[1][$ind];
+                echo "</article>";
+            }
+            ?>
+            <h4>Mostrar Resumen:</h4> <input type="submit" name="resumenAerolinea" value="Enviar">
+        </form>
 
     </body>
 </html>
