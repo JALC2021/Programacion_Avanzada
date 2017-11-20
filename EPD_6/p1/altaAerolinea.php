a5608d11d6cb6f9a8d4ab11ae392e497022cdbca
jalc <jantonio2511@gmail.com>
2017-11-20 18:28:26 +0100

    epd6_p1
    estilos

---
+++ EPD_6/p1/altaAerolinea.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+        <title>Epd_6_p1</title>
+    </head>
+    <body>
+        <?php
+        echo 'altaAerolinea';
+        ?>
+    </body>
+</html>
--- EPD_6/p1/index.php
+++ EPD_6/p1/index.php
@@ -6,12 +6,25 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+        <title>Epd_6_p1</title>
     </head>
     <body>
+        <form method="get" action=".">
+            <input type="submit" name="altaAerolinea" value="Alta Aerolinea"> 
+            <input type="submit" name="altaVuelos" value="Alta Vuelos"> 
+            <input type="submit" name="informeResumen" value="Informe Resumen"> 
+        </form>
         <?php
-       echo "2";
+        if (isset($_GET['altaAerolinea'])) {
+            require './altaAerolinea.php';
+        }
+        if (isset($_GET['altaVuelos'])) {
+            require './altaVuelos.php';
+        }
+        if (isset($_GET['informeResumen'])) {
+            require './informeResumen.php';
+        }
         ?>
     </body>
 </html>
