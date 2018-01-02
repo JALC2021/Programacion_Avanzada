1199dbcfa2efa86d04e873da14c7cfa088ff9f04
jalc <jantonio2511@gmail.com>
2018-01-02 22:42:29 +0100

    trabajo
    vista principal casi terminada

---
+++ HealthySocial/healthySocial/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HealthtyLife</title>
+        <meta name="viewport" content="width=device-width, user-scalabe=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Healthy Social</h1>
+        </header>
+
+        <div class="contendioPrincipal">
+
+            <nav class="menu">
+                <a href="#" id="botonMenu">MEN&Uacute;</a>
+                <a href="#">PAGINA PERSONAL</a>
+                <a href="#">Publicar (formulario)</a>
+                <a href="#">Modificar datos personales</a>
+                <a href="#">Consultar estadísticas</a>
+                <a href="#">Darse de baja</a>
+                <a href="#">PUBLICACIONES AMIGOS</a>
+                <a href="#">Formulario de busqueda</a>
+                <a href="#">Consultar estadísticas</a>
+                <a href="#">PUBLICACIONES AMIGOS</a>
+                <a href="#">BUSCAR AMIGOS</a>
+                <a href="#"id="botonCerrarSesion">CERRAR SESI&Oacute;N</a>
+            </nav>
+
+            <section id="section">
+
+                <article>
+                    <img class="imagenesArticulos" src="https://guiafitness.com/wp-content/uploads/tipos-deportes-invierno.jpg" alt="deporte">
+
+                </article>
+
+                <article>
+
+                    <img class="imagenesArticulos"src="http://ideasdeeventos.com/wp-content/uploads/2014/10/vida-sana-recetas-vegetarianas1.jpg" alt="comidaSana">
+
+                </article>
+
+                <article>
+                    <img class="imagenesArticulos" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuWgUVl2yOjS85pcgImwvW2VIBMf8O29aF2mPX7JxAt5S5M3abqA" alt="vidaSana">
+                </article>
+                
+            </section>
+
+
+            <aside>
+           
+                <img class="imagenesAside" src="https://pbs.twimg.com/profile_images/771267606456053765/GdDcSuoM.jpg" alt="vidasana">
+                <img class="imagenesAside" src="http://wellness.syr.edu/wp-content/uploads/2016/06/healthy-habits-lifestyle-300x225.jpg"alt="vidasana">
+                <img class="imagenesAside" src="http://www.infanciayeducacion.com/wp-content/uploads/2014/05/Platoni%C3%B1os.jpg" alt="vidasana">
+                <img class="imagenesAside" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE4TLEuIURtPfCWJyJauOqthYuZr46dZn8RI98d_YjOPZMeFAhQw" alt="vidasana">
+                <img class="imagenesAside" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDASzryrOW4mg0ipwe34h7It9j0_vjncRWv9BqIoauRXK-Jypm" alt="vidasana">
+                <img class="imagenesAside" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-77LWgwBHvySki92X-uhK-R1afpz6_WWmDNuJHVaZ-iZdoWl_" alt="vidasana">
+            </aside>
+        </div>
+
+        <footer>
+            <h3>Footer</h3>
+        </footer>
+    </body>
+</html>
