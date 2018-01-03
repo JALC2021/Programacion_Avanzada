43a54e28c7f661f8ac555807fbff1fa8025ede2b
jalc <jantonio2511@gmail.com>
2018-01-03 13:46:43 +0100

    trabajo
    mi parte lista

--- HealthySocial/healthySocial/index.php
+++ HealthySocial/healthySocial/index.php
@@ -10,8 +10,11 @@ and open the template in the editor.
         <title>HealthtyLife</title>
         <meta name="viewport" content="width=device-width, user-scalabe=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
     <body>
+
         <header>
             <h1>Healthy Social</h1>
         </header>
@@ -19,7 +22,7 @@ and open the template in the editor.
         <div class="contendioPrincipal">
 
             <nav class="menu">
-                <a href="#" id="botonMenu">MEN&Uacute;</a>
+                <a href="#" id="botonMenu" class="material-icons">MEN&Uacute; &#xe7fd; </a>
                 <a href="#">PAGINA PERSONAL</a>
                 <a href="#">Publicar (formulario)</a>
                 <a href="#">Modificar datos personales</a>
@@ -30,31 +33,38 @@ and open the template in the editor.
                 <a href="#">Consultar estadísticas</a>
                 <a href="#">PUBLICACIONES AMIGOS</a>
                 <a href="#">BUSCAR AMIGOS</a>
-                <a href="#"id="botonCerrarSesion">CERRAR SESI&Oacute;N</a>
+                <a href="#"id="botonCerrarSesion"<button><i class="material-icons">CERRAR SESI&Oacute;N power_settings_new</i></button></a>
             </nav>
 
-            <section id="section">
+            <section class="section">
 
                 <article>
                     <img class="imagenesArticulos" src="https://guiafitness.com/wp-content/uploads/tipos-deportes-invierno.jpg" alt="deporte">
-
+                    <button style="font-size:24px"><i class="fa fa-thumbs-o-up"></i></button>
+                    <button style="font-size:24px">Total <i class="fa fa-heart"></i></button>
+                    <button style="font-size:24px"><i class="fa fa-thumbs-o-down"></i></button>
+                    <button style="font-size:24px"><i class="fa fa-align-justify"></i></button>
                 </article>
 
                 <article>
-
                     <img class="imagenesArticulos"src="http://ideasdeeventos.com/wp-content/uploads/2014/10/vida-sana-recetas-vegetarianas1.jpg" alt="comidaSana">
-
+                    <button style="font-size:24px"><i class="fa fa-thumbs-o-up"></i></button>
+                    <button style="font-size:24px">Total <i class="fa fa-heart"></i></button>
+                    <button style="font-size:24px"><i class="fa fa-thumbs-o-down"></i></button>
+                    <button style="font-size:24px"><i class="fa fa-align-justify"></i></button>
                 </article>
 
                 <article>
                     <img class="imagenesArticulos" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuWgUVl2yOjS85pcgImwvW2VIBMf8O29aF2mPX7JxAt5S5M3abqA" alt="vidaSana">
+                    <button style="font-size:24px"><i class="fa fa-thumbs-o-up"></i></button>
+                    <button style="font-size:24px">Total <i class="fa fa-heart"></i></button>
+                    <button style="font-size:24px"><i class="fa fa-thumbs-o-down"></i></button>
+                    <button style="font-size:24px"><i class="fa fa-align-justify"></i></button>
                 </article>
-                
+
             </section>
 
-
-            <aside>
-           
+            <aside class="aside">
                 <img class="imagenesAside" src="https://pbs.twimg.com/profile_images/771267606456053765/GdDcSuoM.jpg" alt="vidasana">
                 <img class="imagenesAside" src="http://wellness.syr.edu/wp-content/uploads/2016/06/healthy-habits-lifestyle-300x225.jpg"alt="vidasana">
                 <img class="imagenesAside" src="http://www.infanciayeducacion.com/wp-content/uploads/2014/05/Platoni%C3%B1os.jpg" alt="vidasana">
@@ -64,8 +74,8 @@ and open the template in the editor.
             </aside>
         </div>
 
-        <footer>
-            <h3>Footer</h3>
+        <footer class="footer">
+            <h3>Gonzalo - Juan Antonio - Susana</h3>
         </footer>
     </body>
 </html>
