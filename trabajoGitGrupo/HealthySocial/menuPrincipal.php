041fdb2397fa665dfac599c1aa58357f5f1421ce
jalc <jantonio2511@gmail.com>
2018-01-09 18:30:44 +0100

    trabajoGrupo

---
+++ trabajoGitGrupo/HealthySocial/menuPrincipal.php
@@ -0,0 +1,11 @@
+<nav class="menu">
+    <a href="./index.php" id="botonMenu" class="material-icons">MEN&Uacute; &#xe7fd; </a>
+    <a href="#">PAGINA PERSONAL</a>
+    <a href="./formularioContenido.php">Publicar contenido</a>
+    <a href="#">Modificar datos personales</a>
+    <a href="./estadistica.php">Consultar estadísticas</a>
+    <a href="#">Darse de baja</a>
+    <a href="#">PUBLICACIONES AMIGOS</a>
+    <a href="#">BUSCAR AMIGOS</a>
+    <a href="logout.php"id="botonCerrarSesion"<button><i class="material-icons">CERRAR SESI&Oacute;N power_settings_new</i></button></a>
+</nav>
